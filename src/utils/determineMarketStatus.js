2d86213595edfc276b6b46fdfd7062aa3771e816
Deepak Jayaram <deepak-jayaram@live.com>
2020-07-26 13:24:57 -0400

    Adding logic around market status

--- src/utils/determineMarketStatus.js
+++ src/utils/determineMarketStatus.js
@@ -1,3 +1,15 @@
 export const determineMarketStatus = (date) =>  {
- return date.toLocaleString("en-US", {timeZone: "America/New_York"});
+	const currentTimeInNY = date.toLocaleString("en-US", {timeZone: "America/New_York"});
+	const dateFromNYTime = new Date(currentTimeInNY);
+	const currentDay = dateFromNYTime.getDay();
+	const currentHour = dateFromNYTime.getHours();
+	const currentMinute = dateFromNYTime.getMinutes();
+
+	// check if weekend or not inside operating hours (9:30Am to 5:00PM), return closed
+	if(currentDay === 6 || currentDay === 0) {
+		return 'closed';
+	} else if(((currentHour < 9) || (currentHour == 9 && currentMinute < 30)) || currentHour > 17 ) {
+		return 'closed'
+	}
+		return 'open';
 }
